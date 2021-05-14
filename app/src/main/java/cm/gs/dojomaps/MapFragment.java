0a7038662e6a29281f65b024bb5a7f16d902e93c
juancr5 <juancr5@hotmail.com>
2021-05-14 01:59:09 -0500

    Couple change settings

--- app/src/main/java/cm/gs/dojomaps/MapFragment.java
+++ app/src/main/java/cm/gs/dojomaps/MapFragment.java
@@ -1,21 +1,24 @@
 package cm.gs.dojomaps;
 
+import android.location.Location;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import cm.gs.dojomaps.Providers.LocationProvider;
 
 
 public class MapFragment extends Fragment {
 
+    LocationProvider mLocation;
 
+    cm.gs.dojomaps.models.Location lastLocation = new cm.gs.dojomaps.models.Location();
 
     public MapFragment() {
         // Required empty public constructor
@@ -26,7 +29,7 @@ public class MapFragment extends Fragment {
                              Bundle savedInstanceState) {
         //Inicializamos la vista
         View view = inflater.inflate(R.layout.fragment_map, container, false);
-
+        mLocation = new LocationProvider(getActivity());
         //Inicializamos el map fragment
         SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.google_map);
 
@@ -35,6 +38,14 @@ public class MapFragment extends Fragment {
             @Override
             public void onMapReady(GoogleMap googleMap) {
                 //Esta parte sucede cuando el mapa esté cargado
+                mLocation.getLastLocation(getActivity(),getContext()).addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        lastLocation.setLatitude(location.getLatitude());
+                        lastLocation.setLongitude(location.getLongitude());
+                        mLocation.addMarker(googleMap, lastLocation);
+                    }
+                });
             }
         });
 
--- app/src/main/java/cm/gs/dojomaps/Providers/LocationProvider.java
+++ app/src/main/java/cm/gs/dojomaps/Providers/LocationProvider.java
@@ -5,12 +5,13 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -25,8 +26,12 @@ public class LocationProvider {
     }
 
     public Task<Location> getLastLocation(Activity activity, Context context) {
-        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            Toast.makeText(activity, "No adquiriste permisos", Toast.LENGTH_SHORT).show();
+        if (ActivityCompat.checkSelfPermission(context
+                , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(context
+                , Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(activity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    , Manifest.permission.ACCESS_COARSE_LOCATION},44);
         }
         return fusedLocationClient.getLastLocation();
     }
@@ -34,6 +39,7 @@ public class LocationProvider {
     public void addMarker(GoogleMap googleMap, cm.gs.dojomaps.models.Location location) {
         LatLng lastLocation = new LatLng(location.getLatitude(), location.getLongitude());
         googleMap.addMarker(new MarkerOptions().position(lastLocation).title("Estoy acá"));
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(lastLocation, 10));
     }
 
 }
